54fd77fe8aef0e42f4764bb2cb11d7d42458af0a
Juan Palacios <palaciosgalvanj@gmail.com>
2018-12-07 18:27:05 +0100

    New fields added to Booking entity

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -17,6 +17,31 @@ class Booking
     private $id;
 
     /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\House", inversedBy="bookings")
+     */
+    private $house;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $phone;
+
+    /**
      * @ORM\Column(type="date")
      */
     private $fromDate;
@@ -29,21 +54,76 @@ class Booking
     /**
      * @ORM\Column(type="integer")
      */
-    private $numberOfGuests;
+    private $numberOfAdults;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="integer")
      */
-    private $email;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\House", inversedBy="bookings")
-     */
-    private $house;
+    private $numberOfKids;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getHouse(): House
+    {
+        return $this->house;
+    }
+
+    public function setHouse(House $house): self
+    {
+        $this->house = $house;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
     }
 
     public function getFromDate(): ?\DateTimeInterface
@@ -70,39 +150,28 @@ class Booking
         return $this;
     }
 
-    public function getNumberOfGuests(): ?int
+    public function getNumberOfAdults(): ?int
     {
-        return $this->numberOfGuests;
+        return $this->numberOfAdults;
     }
 
-    public function setNumberOfGuests(int $numberOfGuests): self
+    public function setNumberOfAdults(int $numberOfAdults): self
     {
-        $this->numberOfGuests = $numberOfGuests;
+        $this->numberOfAdults = $numberOfAdults;
 
         return $this;
     }
 
-    public function getEmail(): ?string
+    public function getNumberOfKids(): ?int
     {
-        return $this->email;
+        return $this->numberOfKids;
     }
 
-    public function setEmail(string $email): self
+    public function setNumberOfKids(int $numberOfKids): self
     {
-        $this->email = $email;
+        $this->numberOfKids = $numberOfKids;
 
         return $this;
     }
 
-    public function getHouse(): House
-    {
-        return $this->house;
-    }
-
-    public function setHouse(House $house): self
-    {
-        $this->house = $house;
-
-        return $this;
-    }
 }
--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -6,7 +6,9 @@ use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -16,10 +18,52 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('fromDate', DateType::class)
-            ->add('toDate', DateType::class)
-            ->add('numberOfGuests', NumberType::class)
+            ->add('firstName', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nombre'
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Apellido'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Correo electrónico'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'attr' => [
+                    'placeholder' => 'Teléfono'
+                ]
+            ])
+            ->add('fromDate', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+                'attr' => [
+                    'placeholder' => 'Día de entrada'
+                ],
+                'html5' => false
+            ])
+            ->add('toDate', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+                'attr' => [
+                    'placeholder' => 'Día de salida'
+                ],
+                'html5' => false
+            ])
+            ->add('numberOfAdults', NumberType::class, [
+                'attr' => [
+                    'placeholder' => 'Adultos'
+                ]
+            ])
+            ->add('numberOfKids', NumberType::class, [
+                'attr' => [
+                    'placeholder' => 'Niños (hasta 10 años)'
+                ]
+            ])
         ;
     }
 
